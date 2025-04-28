52abe151bba839c7a8c434eb080b940744a4ab04
AKSHANSH KUMAR TIWARI <akshansh845430@gmail.com>
2025-04-28 23:35:05 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 
 import { UserContextProvider } from "./context/userContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
-export const server = "http://localhost:3000";
+export const server = "https://e-learn-backend-sbet.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
